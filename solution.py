6a8e990ca96ec8fe38cfee5e58da886c0d946b23
Alamgeer-official <77409907+Alamgeer-official@users.noreply.github.com>
2021-03-10 19:58:08 +0530

    Add files via upload

---
+++ solution.py
@@ -0,0 +1,49 @@
+#!/bin/python3
+
+import math
+import os
+import random
+import re
+import sys
+
+#
+# Complete the 'diagonalDifference' function below.
+#
+# The function is expected to return an INTEGER.
+# The function accepts 2D_INTEGER_ARRAY arr as parameter.
+#
+
+def diagonalDifference(arr):
+    prim =0
+    sec=0
+    i=0
+    # d=len(arr[0])
+    # while(i<=len(arr[0])):
+    #     prim+=arr[i][i]
+    #     sec+=arr[i][d]
+    #     i=i+1
+    #     d=d-1
+    # return abs(prim-sec)
+        
+    length = len(arr[0])
+    for count in range(length):
+        prim += arr[count][count]
+        sec += arr[count][(length-count-1)]
+    return abs(prim-sec)
+
+
+if __name__ == '__main__':
+    fptr = open(os.environ['OUTPUT_PATH'], 'w')
+
+    n = int(input().strip())
+
+    arr = []
+
+    for _ in range(n):
+        arr.append(list(map(int, input().rstrip().split())))
+
+    result = diagonalDifference(arr)
+
+    fptr.write(str(result) + '\n')
+
+    fptr.close()
